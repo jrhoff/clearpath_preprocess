059f6a6e6960d5c58148b71656bb5c0e525f1f7b
root <root@ip-172-31-4-118.us-east-2.compute.internal>
2021-04-12 20:15:37 +0000

    added setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip3 install flask
+pip3 install requests
+pip3 install nltk
+pip3 install spacy
+pip3 install scispacy
+pip3 install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz
