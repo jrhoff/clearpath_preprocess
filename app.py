833ff63b2379284e1e5d113a75551971fb107237
jrhoff <jrhoff@pop-os.localdomain>
2021-05-17 17:31:44 -0400

    fixed endpoint parsing, change to json

--- app.py
+++ app.py
@@ -20,21 +20,11 @@ def get_endpoints():
         for line in f.readlines():
             tupl = line.strip().split()
             endpoints[tupl[0]] = tupl[1]
-    PRODUCTION_ENDPOINT = endpoints['production']
-    TEST_ENDPOINT = endpoints['test']
-    return endpoints
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    
+    return endpoints['production'], endpoints['test']
 
 
-@app.route('/submission_form')
-def submission():
-    return render_template('upload.html')
-
-
-@app.route('/handle_submission', methods=['POST'])
+@app.route('/', methods=['POST'])
 def split():
     """
     This endpoint expects:
@@ -42,27 +32,25 @@ def split():
         2 - an email address for the patient/submitter of pathology report
     :return:
     """
-    if request.method == 'POST':
-        
-        f = request.files['file']
-        print(request.form)
-        email = request.form['email']
-        text = f.read().decode("utf-8")
-        # Remove PHI
-        de_identified_text = filter_task(text, scispacy)
+    PRODUCTION_ENDPOINT, TEST_ENDPOINT = get_endpoints()
+    content = request.get_json()
+    text, email = content['text'], content['email'].strip()
 
-        # Clean text
-        cleaner = Cleaner(de_identified_text)
-        cleaned_text = cleaner.text
+    # Remove PHI
+    de_identified_text = filter_task(text, scispacy)
 
-        # Finally preprocess
-        preprocessor = SpacyPreProcessor(scispacy)
-        text, tokens_list = preprocessor.preprocess_sentences(cleaned_text)
-        m = {'text': text, 'tokens': tokens_list, 'email': email}
-        response = requests.post(url=PRODUCTION_ENDPOINT, json=m)
-        return response.json()
+    # Clean text
+    cleaner = Cleaner(de_identified_text)
+    cleaned_text = cleaner.text
 
-    return 0
+    # Finally preprocess
+    preprocessor = SpacyPreProcessor(scispacy)
+    text, tokens_list = preprocessor.preprocess_sentences(cleaned_text)
+    m = {'text': text, 'tokens': tokens_list, 'email': email}
+    response = requests.post(url=TEST_ENDPOINT, json=m)
+    return response.json()
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
